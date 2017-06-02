656d670816edee5b38ea1e856d1395f75b8d715b
hanlc <282552154@qq.com>
2017-06-02 17:25:28 +0800

    增加sub test

---
+++ examples/sub_test.php
@@ -0,0 +1,23 @@
+<?php
+/*
+* Pubsub envelope subscriber
+* @author Ian Barber <ian(dot)barber(at)gmail(dot)com>
+*/
+
+//  Prepare our context and subscriber
+$context = new ZMQContext();
+$subscriber = new ZMQSocket($context, ZMQ::SOCKET_SUB);
+$subscriber->connect("tcp://127.0.0.1:7777");
+$subscriber->setSockOpt(ZMQ::SOCKOPT_SUBSCRIBE, "logger");
+
+while (true) {
+    //  Read envelope with address
+    $address = $subscriber->recv();
+    //  Read message contents
+    $contents = $subscriber->recv();
+    printf ("[%s] %s%s", $address, $contents, PHP_EOL);
+}
+//  We never get here
+
+
+
